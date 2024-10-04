1fd04e521b35dea1928c6ccd2914169b8adc0329
Jodok Huber <jodokh@gmail.com>
2024-10-04 16:36:41 +0200

    updated dist folder for new clipping inside feature

--- dist/materials/clipping.d.ts
+++ dist/materials/clipping.d.ts
@@ -2,7 +2,8 @@ import { Box3, Matrix4, Vector3 } from 'three';
 export declare enum ClipMode {
     DISABLED = 0,
     CLIP_OUTSIDE = 1,
-    HIGHLIGHT_INSIDE = 2
+    CLIP_INSIDE = 2,
+    HIGHLIGHT_INSIDE = 3
 }
 export interface IClipBox {
     box: Box3;
